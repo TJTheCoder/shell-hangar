02006d01cbab6e1acbce9233c84073ce32ae7591
Tanuj Tekkale <tanujt2k5@gmail.com>
2025-12-19 18:52:22 -0600

    word fix

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -53,7 +53,7 @@ export function LoginForm({
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email below to log into your account.
           </CardDescription>
         </CardHeader>
         <CardContent>
